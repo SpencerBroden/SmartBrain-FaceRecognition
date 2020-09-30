d29e12fa57a052aca1f6bccc7a976c1fa64a3e46
Spencer Broden <9060485+SpencerBroden@users.noreply.github.com>
2020-09-30 15:23:34 -0700

    Updates

---
+++ precache-manifest.e702a8edc7e492246992a504b4217480.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d600fd7d71104c1dc040e9b128c2f467",
+    "url": "/SmartBrain-FaceRecognition/index.html"
+  },
+  {
+    "revision": "fb291551fbb0273c96a0",
+    "url": "/SmartBrain-FaceRecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "fee4125f8a9554631bbd",
+    "url": "/SmartBrain-FaceRecognition/static/css/main.321c06f7.chunk.css"
+  },
+  {
+    "revision": "fb291551fbb0273c96a0",
+    "url": "/SmartBrain-FaceRecognition/static/js/2.4ca91bdd.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/SmartBrain-FaceRecognition/static/js/2.4ca91bdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fee4125f8a9554631bbd",
+    "url": "/SmartBrain-FaceRecognition/static/js/main.eb73974c.chunk.js"
+  },
+  {
+    "revision": "65f0f9b77f0c67e993e1",
+    "url": "/SmartBrain-FaceRecognition/static/js/runtime-main.9baec025.js"
+  },
+  {
+    "revision": "d775676b2ac8bb2c969bce3db3887d09",
+    "url": "/SmartBrain-FaceRecognition/static/media/brain.d775676b.png"
+  }
+]);
